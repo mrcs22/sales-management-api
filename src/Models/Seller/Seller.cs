a629ac25b2f9b214f18fbe052fe054302341477e
mrcs <marcusmoraes2010@hotmail.com>
2023-01-16 20:19:24 -0300

    feat: create Seller Model

---
+++ src/Models/Seller/Seller.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace tech_test_payment_api.Models.Seller
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(11)]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "cpf must be numeric")]
+        public string Cpf { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+       
+       [Required]
+       [MinLength(10)]
+       [MaxLength(11)]
+       [RegularExpression("^[0-9]*$", ErrorMessage = "phoneNumber must be numeric")]
+        public string PhoneNumber { get; set; }
+    }
+}
