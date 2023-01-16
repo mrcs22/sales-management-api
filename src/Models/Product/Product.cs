cf0421301e3a13db7718195251555a7f39581575
mrcs <marcusmoraes2010@hotmail.com>
2023-01-16 20:25:12 -0300

    feat: create Product Model

---
+++ src/Models/Product/Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tech_test_payment_api.Models.Product
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int stock { get; set; }
+    }
+}
