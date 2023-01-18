12e4d7f859138b625e2bd7069e5e5513d323426c
mrcs <marcusmoraes2010@hotmail.com>
2023-01-17 21:09:58 -0300

    feat: create service to approve Sale payment

---
+++ src/Services/Sale/SaleService.cs
@@ -0,0 +1,52 @@
+using tech_test_payment_api.Models;
+using tech_test_payment_api.Repositories;
+using tech_test_payment_api.Services.Exceptions;
+
+namespace tech_test_payment_api.Services
+{
+    public class SaleService : ISaleService
+    {
+        private readonly ISaleRepository _saleRepository;
+
+        public SaleService(ISaleRepository saleRepository)
+        {
+            _saleRepository = saleRepository;
+        }
+        public void ApproveSalePayment(int id)
+        {
+            var sale = _saleRepository.GetSaleById(id);
+
+            if(sale.Status != EnumStatusSale.Waiting_payment)
+                throw new SaleServiceException($"Sale of id {id} is not waiting for payment");
+            
+            sale.Status = EnumStatusSale.Payment_accepted;
+
+            _saleRepository.UpdateSale(sale);
+        }
+
+        public void CancelSale(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSale(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinishSaleDelivery(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sale GetSaleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendSaleToCarrier(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
