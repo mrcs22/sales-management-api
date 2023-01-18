64589941664ed9639e77328dcb44108567fb30f2
mrcs <marcusmoraes2010@hotmail.com>
2023-01-17 23:39:09 -0300

    refactor: set DataAnottations to define Seller, and Products as required in Sale Model

--- src/Models/Sale/Sale.cs
+++ src/Models/Sale/Sale.cs
@@ -1,4 +1,4 @@
-
+using System.ComponentModel.DataAnnotations;
 namespace tech_test_payment_api.Models
 {
     public class Sale
@@ -6,8 +6,10 @@ namespace tech_test_payment_api.Models
         public int Id { get; set; }
         public string OrderIdentifier { get; set; }
         public DateTime Date { get; set; }
+        [Required]
         public Seller Seller { get; set; }
         public EnumStatusSale Status {get; set;}
-        public Product[] Products { get; set; }
+        [Required]
+        public List<Product> Products { get; set; }
     }
 }
