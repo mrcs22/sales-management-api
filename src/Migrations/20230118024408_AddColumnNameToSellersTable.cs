c4441f1269f13a1ff9a8331ae4a46e796b49f307
mrcs <marcusmoraes2010@hotmail.com>
2023-01-17 23:49:00 -0300

    feat: add migrations to add name column to Sellers table

---
+++ src/Migrations/20230118024408_AddColumnNameToSellersTable.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace techtestpaymentapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnNameToSellersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Sellers_SellerId",
+                table: "Sales");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Sellers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SellerId",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Sellers_SellerId",
+                table: "Sales",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Sellers_SellerId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Sellers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SellerId",
+                table: "Sales",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Sellers_SellerId",
+                table: "Sales",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id");
+        }
+    }
+}
