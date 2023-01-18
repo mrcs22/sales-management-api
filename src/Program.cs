603ebd476532dce7b06cc582da97d2cd7e7f6052
mrcs <marcusmoraes2010@hotmail.com>
2023-01-17 21:49:45 -0300

    feat: configure dependency injection

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using tech_test_payment_api.Data;
+using tech_test_payment_api.Repositories;
+using tech_test_payment_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(Environment.GetEnvironmentVariable("DbConnectionString")));
+
+builder.Services.AddTransient<ISaleRepository, SaleRepository>();
+builder.Services.AddTransient<ISaleService, SaleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
