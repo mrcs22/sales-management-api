82c885cd2933db32cfa2804941e8a1c55c2680be
mrcs <marcusmoraes2010@hotmail.com>
2023-01-17 23:41:39 -0300

    refactor: set DataAnottations to define name as required, and amount as required and greater than 0 in Product Model

--- src/Models/Product/Product.cs
+++ src/Models/Product/Product.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 namespace tech_test_payment_api.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Amount { get; set; }
 
         public DateTime CreatedAt {get; set;}
