cf27640dee7d771c6480f2342db7222d0c3017b8
mrcs <marcusmoraes2010@hotmail.com>
2023-01-18 21:07:11 -0300

    test: add unit tests for getSaleById method of SalesController

---
+++ tests/Sale/SalesControllerTest.cs
@@ -0,0 +1,46 @@
+using Moq;
+using tech_test_payment_api.Models;
+using tech_test_payment_api.Services;
+using tech_test_payment_api.Controllers;
+using tests.Factories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tests
+{
+    public class SalesControllerTest
+    {
+        private readonly Mock<ISaleService> _mockSaleService;
+        public SalesControllerTest()
+        {
+            _mockSaleService = new Mock<ISaleService>();
+        }
+
+        [Fact]
+        public void ShouldReturnSaleWithOkStatusWhenGetSaleByIdWithValidId()
+        {
+            var expectedSale = SaleFactory.CreateValidSale(EnumStatusSale.Waiting_payment);
+            _mockSaleService.Setup(s => s.GetSaleById(expectedSale.Id)).Returns(expectedSale);
+
+            var saleController = new SalesController(_mockSaleService.Object);
+
+            var result = saleController.GetSaleById(expectedSale.Id) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedSale, result.Value);
+        }
+
+        [Fact]
+        public void ShouldReturnNotFoundStatusWhenGetSaleByIdWithInvalidId()
+        {
+            var saleController = new SalesController(_mockSaleService.Object);
+
+            int invalidSaleId = 0;
+            var result = saleController.GetSaleById(invalidSaleId) as NotFoundResult;
+
+            Assert.NotNull(result);
+
+            int notFoundStatusCode = 404;
+            Assert.Equal(notFoundStatusCode, result.StatusCode);
+        }
+    }
+}
